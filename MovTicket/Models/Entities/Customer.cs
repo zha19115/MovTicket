69f4c79da39171863bc33b805fce577e7ff94fa6
ANNI_ZHAN <zha19115@spengergasse.at>
2024-08-22 23:00:45 +0200

    Revert "Small changes only"
    This reverts commit 79d293de65ec67d41e2ac04a19731da45d51df53.

--- MovTicket/Models/Entities/Customer.cs
+++ MovTicket/Models/Entities/Customer.cs
@@ -7,17 +7,17 @@ namespace MovTicket.Models.Entities
         [Key]
         public int c_id { get; set; }
         public string c_name { get; set; }
-        public string c_adress { get; set; }
+        public string c_address { get; set; }
         public string c_email { get; set; } = string.Empty;
         public string c_phone { get; set; } = string.Empty;
         public bool c_subscription { get; set; }
 
 
-        /*public Customer(int id, string name, string adress, string email, string phone, bool subscription)
+        /*public Customer(int id, string name, string address, string email, string phone, bool subscription)
         {
             id = c_id;
             name = c_name;
-            adress = c_adress;
+            address = c_address;
             email = c_email;
             phone = c_phone;
             subscription = c_subscription;
